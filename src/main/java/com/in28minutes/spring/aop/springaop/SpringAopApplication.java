ef12fa16cf58cc0ff5329cbab397435f74241190
mrashidi <m.rashidi@sadad.co.ir>
2020-02-10 18:09:05 +0330

    Second Commit

--- src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
@@ -8,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.in28minutes.spring.aop.springaop")
 public class SpringAopApplication implements CommandLineRunner {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
